fb032dae76d2f0ae882b8b1fbdbce5115956ce9c
Cukoo <liwwcukoo@gmail.com>
2024-07-11 09:41:42 +0800

    Update buffer.h

--- src/buffer/buffer.h
+++ src/buffer/buffer.h
@@ -1,48 +1,44 @@
 #ifndef BUFFER_H
 #define BUFFER_H
 
+#include <assert.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <atomic>
 #include <cstring>
 #include <iostream>
-#include <unistd.h>
-#include <sys/uio.h>
 #include <vector>
-#include <atomic>
-#include <assert.h>
 
 class Buffer {
-    public:
+   public:
     Buffer(int init_buff_size = 1024);
     ~Buffer() = default;
 
-    size_t WritableBytes() const;
+    size_t PrependableBytes() const;
     size_t ReadableBytes() const;
-    size_t PrependableBytes() const;    
+    size_t WritableBytes() const;
 
-    const char* ReadBegin() const;   
-    void EnsureWritable(size_t len);    
-    void HasWritten(size_t len);    
+    const char* ReadBegin() const;
+    const char* ConstWriteBegin() const;
 
-    void Retrieve(size_t len);    
-    void RetrieveUntil(const char* end);    
-
-    void RetrieveAll();        
-    std::string RetrieveAlltoStr();     
-
-    const char* ConstWriteBegin() const;   
-    char* WriteBegin();
+    void Retrieve(size_t len);
+    void RetrieveUntil(const char* end);
+    void RetrieveAll();
 
     void Append(const std::string& str);
     void Append(const char* str, size_t len);
-    // void Append(const void* data, size_t len);
     void Append(const Buffer& buff);
 
     ssize_t ReadFd(int fd, int* save_errno);
     ssize_t WriteFd(int fd, int* save_errno);
 
-    private:
+   private:
     char* Begin();
     const char* Begin() const;
+    char* WriteBegin();
+    void EnsureWritable(size_t len);
     void MakeSpace(size_t len);
+    void HasWritten(size_t len);
 
     std::vector<char> buffer_;
     std::atomic<size_t> read_pos_;
